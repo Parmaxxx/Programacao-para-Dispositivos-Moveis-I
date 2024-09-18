7c54f97bc27fcc599f81ff25502b5984f19f25e7
Ricardo <ric_msouza@hotmail.com>
2024-09-18 09:29:00 -0300

    Utilizando Recicle View

---
+++ Aula07/app/src/main/java/com/example/aula_07/activity/ContatoListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aula_07.activity
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.aula_06.R
+
+class ContatoListActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.contato_lista_activity)
+    }
+}
