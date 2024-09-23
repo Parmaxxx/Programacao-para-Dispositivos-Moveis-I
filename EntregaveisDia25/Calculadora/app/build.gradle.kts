0415d912a23b79b11f91666011e43f88cdab524f
Ricardo <ric_msouza@hotmail.com>
2024-09-22 22:17:40 -0300

    Calculadora(apenas Layout)

--- EntregaveisDia25/Calculadora/app/build.gradle.kts
+++ EntregaveisDia25/Calculadora/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.agenda"
+    namespace = "com.example.calculadora"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.agenda"
+        applicationId = "com.example.calculadora"
         minSdk = 27
         targetSdk = 34
         versionCode = 1
@@ -27,21 +27,20 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "1.8"
     }
-
 }
 
 dependencies {
 
-    implementation(libs.appcompat)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.core.ktx)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
---
+++ EntregaveisDia25/Calculadora/app/src/main/java/com/example/calculadora/CalculadoraActivity.kt
@@ -0,0 +1,11 @@
+package com.example.calculadora
+
+import android.app.Activity
+import android.os.Bundle
+
+class CalculadoraActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.calculadora_layout)
+    }
+}
